1a5af42f177f5c386dd068b6c41a3959df93d74d
Benjamin Jordan <benjamin@thegoldenmule.com>
2021-01-07 17:44:33 -0600

    Moved encryption primitives to static file.

--- zk-server/Controllers/EncryptionController.cs
+++ zk-server/Controllers/EncryptionController.cs
@@ -1,10 +1,10 @@
 using System;
-using System.Security.Cryptography;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Zk.Models.Db;
 using Zk.Models.Network;
+using Zk.Services;
 
 namespace Zk.Controllers
 {
@@ -26,20 +26,15 @@ namespace Zk.Controllers
         {
             var publicKey = Convert.FromBase64String(req.PublicKey);
             var plainText = Encoding.UTF8.GetBytes(req.PlainText);
-            
-            using var rsa = RSA.Create();
-            rsa.ImportRSAPublicKey(new ReadOnlySpan<byte>(publicKey), out _);
-
-            var cipherBytes = rsa.Encrypt(plainText, RSAEncryptionPadding.Pkcs1);
-            var cipherText = Convert.ToBase64String(cipherBytes);
+            var cipherBytes = EncryptionUtility.Encrypt(publicKey, plainText);
 
             return new EncryptPayloadResponse
             {
                 Success = true,
-                CipherText = cipherText,
+                CipherText = Convert.ToBase64String(cipherBytes),
             };
         }
-        
+
         [HttpPost]
         [Route("decrypt")]
         public DecryptPayloadResponse Decrypt(DecryptPayloadRequest req)
@@ -47,10 +42,7 @@ namespace Zk.Controllers
             var privateKey = Convert.FromBase64String(req.PrivateKey);
             var cipherBytes = Convert.FromBase64String(req.CipherText);
 
-            using var rsa = RSA.Create();
-            rsa.ImportRSAPrivateKey(new ReadOnlySpan<byte>(privateKey), out _);
-
-            var plainBytes = rsa.Decrypt(cipherBytes, RSAEncryptionPadding.Pkcs1);
+            var plainBytes = EncryptionUtility.Decrypt(privateKey, cipherBytes);
             var plainText = Encoding.UTF8.GetString(plainBytes);
 
             return new DecryptPayloadResponse
@@ -65,21 +57,9 @@ namespace Zk.Controllers
         public SignPayloadResponse Sign(SignPayloadRequest req)
         {
             var cipherBytes = Convert.FromBase64String(req.CipherText);
-
-            byte[] hash;
-            using (var alg = SHA512.Create())
-            {
-                hash = alg.ComputeHash(cipherBytes);
-            }
-            
             var privateKey = Convert.FromBase64String(req.PrivateKey);
-            using var rsa = RSA.Create();
-            rsa.ImportRSAPrivateKey(new ReadOnlySpan<byte>(privateKey), out _);
-            
-            var formatter = new RSAPKCS1SignatureFormatter(rsa);
-            formatter.SetHashAlgorithm("SHA512");
-            
-            var sig = Convert.ToBase64String(formatter.CreateSignature(hash));
+            var sigBytes = EncryptionUtility.Sign(cipherBytes, privateKey);
+            var sig = Convert.ToBase64String(sigBytes);
 
             return new SignPayloadResponse
             {
@@ -94,24 +74,12 @@ namespace Zk.Controllers
         {
             var cipherBytes = Convert.FromBase64String(req.CipherText);
             var sigBytes = Convert.FromBase64String(req.Sig);
-
-            byte[] cipherHash;
-            using (var alg = SHA512.Create())
-            {
-                cipherHash = alg.ComputeHash(cipherBytes);
-            }
-            
             var publicKeyBytes = Convert.FromBase64String(req.PublicKey);
-            using var rsa = RSA.Create();
-            rsa.ImportRSAPublicKey(new ReadOnlySpan<byte>(publicKeyBytes), out _);
-            
-            var deformatter = new RSAPKCS1SignatureDeformatter(rsa);
-            deformatter.SetHashAlgorithm("SHA512");
 
             return new VerifyPayloadResponse
             {
                 Success = true,
-                IsValid = deformatter.VerifySignature(cipherHash, sigBytes)
+                IsValid = EncryptionUtility.IsValidSig(cipherBytes, sigBytes, publicKeyBytes)
             };
         }
     }
---
+++ zk-server/Services/EncryptionUtility.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Security.Cryptography;
+
+namespace Zk.Services
+{
+    public class EncryptionUtility
+    {
+        public static byte[] Encrypt(byte[] publicKey, byte[] plainText)
+        {
+            using var rsa = RSA.Create();
+            rsa.ImportRSAPublicKey(new ReadOnlySpan<byte>(publicKey), out _);
+
+            return rsa.Encrypt(plainText, RSAEncryptionPadding.Pkcs1);
+        }
+
+        public static byte[] Decrypt(byte[] privateKey, byte[] cipherText)
+        {
+            using var rsa = RSA.Create();
+            rsa.ImportRSAPrivateKey(new ReadOnlySpan<byte>(privateKey), out _);
+
+            return rsa.Decrypt(cipherText, RSAEncryptionPadding.Pkcs1);
+        }
+
+        public static byte[] Sign(byte[] cipherText, byte[] privateKey, string alg = "SHA512")
+        {
+            byte[] hash;
+            using (var hashFn = Alg(alg))
+            {
+                hash = hashFn.ComputeHash(cipherText);
+            }
+            
+            using var rsa = RSA.Create();
+            rsa.ImportRSAPrivateKey(new ReadOnlySpan<byte>(privateKey), out _);
+            
+            var formatter = new RSAPKCS1SignatureFormatter(rsa);
+            formatter.SetHashAlgorithm(alg);
+            
+            return formatter.CreateSignature(hash);
+        }
+
+        public static bool IsValidSig(byte[] cipherText, byte[] sig, byte[] publicKey, string alg = "SHA512")
+        {
+            byte[] hash;
+            using (var hashFn = Alg(alg))
+            {
+                hash = hashFn.ComputeHash(cipherText);
+            }
+            
+            using var rsa = RSA.Create();
+            rsa.ImportRSAPublicKey(new ReadOnlySpan<byte>(publicKey), out _);
+            
+            var deformatter = new RSAPKCS1SignatureDeformatter(rsa);
+            deformatter.SetHashAlgorithm(alg);
+
+            return deformatter.VerifySignature(hash, sig);
+        }
+
+        private static HashAlgorithm Alg(string alg)
+        {
+            switch (alg)
+            {
+                default:
+                {
+                    return SHA512.Create();
+                }
+                case "SHA256":
+                {
+                    return SHA256.Create();
+                }
+                case "SHA1":
+                {
+                    return SHA1.Create();
+                }
+            }
+        }
+    }
+}
