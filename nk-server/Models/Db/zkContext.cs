73e12f93bcf9d13aca3f7ea43ef3e68f1ea173d8
Benjamin Jordan <benjamin@thegoldenmule.com>
2021-01-25 13:53:59 -0600

    Lunch tinkering-- add nk to docker compose.

--- nk-server/Models/Db/zkContext.cs
+++ nk-server/Models/Db/zkContext.cs
@@ -23,8 +23,7 @@ namespace TheGoldenMule.Nk.Models.Db
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Database=zk;Username=postgres;Password=example");
+                optionsBuilder.UseNpgsql("Host=db;Database=zk;Username=postgres;Password=example");
             }
         }
 
